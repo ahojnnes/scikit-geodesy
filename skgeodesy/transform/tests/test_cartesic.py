Fix rotation test cases

skgeodesy/transform/tests/test_cartesic.py
@@ -1,14 +1,13 @@
 import numpy as np
 from numpy.testing import run_module_suite, assert_almost_equal
-from skgeodesy import transform
+from skgeodesy import transform, util
 
 
-def test_rotation():
+def test_rotation_init():
     for axis in range(1, 4):
         for angle in np.linspace(-np.pi / 2, np.pi / 2, 10):
-            t = transform.RotationTransform(angle, axis)
+            t = transform.RotationTransform(angle=angle, axis=axis)
             assert_almost_equal(getattr(t, 'angle%d' % axis), angle)
-
 
 
 if __name__ == '__main__':