9931cbd0d849f578bd7f039c063e5b99f77d1b44
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-12-28 15:02:10 +0100

    Add test cases for affine transform

--- skgeodesy/transform/tests/test_cartesic.py
+++ skgeodesy/transform/tests/test_cartesic.py
@@ -16,7 +16,6 @@ class TestTranslationTransform(object):
                 assert_almost_equal(getattr(t, 't%s' % AXIS_NAMES[axis-1]),
                                     translation)
 
-
     def test_call(self):
         t1 = transform.TranslationTransform(translation=0.1, axis=1)
         assert_almost_equal(t1([1, 1, 1]), [1.1, 1, 1])
@@ -26,7 +25,6 @@ class TestTranslationTransform(object):
         assert_almost_equal(t3([1, 1, 1]), [1, 1, 1.1])
         t = t1.before(t2).before(t3)
         assert_almost_equal(t([1, 1, 1]), [1.1, 1.1, 1.1])
-
 
     def test_inverse(self):
         t = transform.TranslationTransform(translation=0.1, axis=1)
@@ -41,8 +39,8 @@ class TestScaleTransform(object):
         for axis in (1, 2, 3):
             for scale in np.linspace(0.1, 4, 10):
                 t = transform.ScaleTransform(scale=scale, axis=axis)
-                assert_almost_equal(getattr(t, 's%s' % AXIS_NAMES[axis-1]), scale)
-
+                tscale = getattr(t, 's%s' % AXIS_NAMES[axis-1])
+                assert_almost_equal(tscale, scale)
 
     def test_call(self):
         t1 = transform.ScaleTransform(scale=0.1, axis=1)
@@ -53,7 +51,6 @@ class TestScaleTransform(object):
         assert_almost_equal(t3([1, 1, 1]), [1, 1, 0.1])
         t = t1.before(t2).before(t3)
         assert_almost_equal(t([1, 1, 1]), [0.1, 0.1, 0.1])
-
 
     def test_inverse(self):
         t = transform.ScaleTransform(scale=0.1, axis=1)
@@ -68,8 +65,8 @@ class TestRotationTransform(object):
         for axis in (1, 2, 3):
             for angle in np.linspace(-np.pi / 2, np.pi / 2, 10):
                 t = transform.RotationTransform(angle=angle, axis=axis)
-                assert_almost_equal(getattr(t, 'r%s' % AXIS_NAMES[axis-1]), angle)
-
+                tangle = getattr(t, 'r%s' % AXIS_NAMES[axis-1])
+                assert_almost_equal(tangle, angle)
 
     def test_call(self):
         # mirror
@@ -96,7 +93,6 @@ class TestRotationTransform(object):
         assert_almost_equal(t([1, 0, 1]), [0, 0, a])
         t = transform.RotationTransform(angle=np.pi / 4, axis=3)
         assert_almost_equal(t([1, 1, 0]), [a, 0, 0])
-
 
     def test_inverse(self):
         t = transform.RotationTransform(angle=1, axis=1)
@@ -132,38 +128,68 @@ class TestShearTransform(object):
 class TestSimilarityTransform(object):
 
     def test_init(self):
-        angles = np.zeros((3, ))
         translation = np.zeros((3, ))
-        for angle in np.linspace(-np.pi / 2, np.pi / 2, 5):
-            angles[:] = angle
+        for rot in np.linspace(-np.pi / 2, np.pi / 2, 5):
+            angle = (rot, rot, rot)
             for scale in np.linspace(0.1, 4, 5):
                 for trans in np.linspace(-100, 100, 10):
                     translation[:] = trans
-                    t = transform.SimilarityTransform(scale=scale, angles=angles,
+                    t = transform.SimilarityTransform(scale=scale, angle=angle,
                                                       translation=translation)
                     assert_almost_equal(t.s, scale)
-                    assert_almost_equal(t.rx, angle)
-                    assert_almost_equal(t.ry, angle)
-                    assert_almost_equal(t.rz, angle)
+                    assert_almost_equal(t.rx, rot)
+                    assert_almost_equal(t.ry, rot)
+                    assert_almost_equal(t.rz, rot)
                     assert_almost_equal(t.tx, trans)
                     assert_almost_equal(t.ty, trans)
                     assert_almost_equal(t.tz, trans)
 
-
     def test_call(self):
-        t = transform.SimilarityTransform(angles=(np.pi, np.pi, np.pi),
+        t = transform.SimilarityTransform(angle=(np.pi, np.pi, np.pi),
                                           translation=(10, 10, 10))
         assert_almost_equal(t([1, 1, 1]), [11, 11, 11])
-        t = transform.SimilarityTransform(scale=2, angles=(np.pi, np.pi, np.pi),
+        t = transform.SimilarityTransform(scale=2, angle=(np.pi, np.pi, np.pi),
                                           translation=(10, 10, 10))
         assert_almost_equal(t([1, 1, 1]), [12, 12, 12])
         t = transform.SimilarityTransform(scale=0.5)
         assert_almost_equal(t([1, 1, 1]), [0.5, 0.5, 0.5])
 
+    def test_inverse(self):
+        t = transform.SimilarityTransform(scale=0.1, angle=(1, 2, 3),
+                                          translation=(1, 2, 3))
+        tinv = t.inverse()
+        coord = [1, 2, 3]
+        assert_almost_equal(coord, tinv(t(coord)))
+
+
+class TestAffineTransform(object):
+
+    def test_init(self):
+        translation = np.zeros((3, ))
+        for rot in np.linspace(-np.pi / 2, np.pi / 2, 5):
+            angle = (rot, rot, rot)
+            for sx in np.linspace(0.1, 4, 5):
+                for trans in np.linspace(-100, 100, 10):
+                    translation[:] = trans
+                    sy = sx + 0.1
+                    sz = sx + 0.2
+                    t = transform.AffineTransform(scale=(sx, sy, sz),
+                                                  angle=angle,
+                                                  translation=translation)
+                    assert_almost_equal(t.sx, sx)
+                    assert_almost_equal(t.sy, sy)
+                    assert_almost_equal(t.sz, sz)
+                    assert_almost_equal(t.rx, rot)
+                    assert_almost_equal(t.ry, rot)
+                    assert_almost_equal(t.rz, rot)
+                    assert_almost_equal(t.tx, trans)
+                    assert_almost_equal(t.ty, trans)
+                    assert_almost_equal(t.tz, trans)
 
     def test_inverse(self):
-        t = transform.SimilarityTransform(scale=0.1, angles=(1, 2, 3),
-                                          translation=(1, 2, 3))
+        t = transform.AffineTransform(scale=(1, 2, 3), angle=(1, 2, 3),
+                                      translation=(1, 2, 3),
+                                      shear=((1, 2), (3, 4), (5, 6)))
         tinv = t.inverse()
         coord = [1, 2, 3]
         assert_almost_equal(coord, tinv(t(coord)))
