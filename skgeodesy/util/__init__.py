79e134ad50c05430e214683d1ac7bfa34f19b55d
Johannes Schönberger <hannesschoenberger@gmail.com>
2013-01-13 15:30:03 +0100

    Add deg2dms and dms2deg conversion functions

--- skgeodesy/util/__init__.py
+++ skgeodesy/util/__init__.py
@@ -1,1 +1,1 @@
-from angle import wrap_to_pi, wrap_to_2pi
+from angle import wrap_to_pi, wrap_to_2pi, deg2dms, dms2deg
--- skgeodesy/util/angle.py
+++ skgeodesy/util/angle.py
@@ -46,3 +46,88 @@ def wrap_to_2pi(angles):
     wrapped[np.logical_and(wrapped == 0, mask)] = 2 * np.pi
 
     return wrapped
+
+
+def deg2dms(x, out=None):
+    """Convert angles in degrees to degrees, minutes and seconds.
+
+    Parameters
+    ----------
+    x : array_like
+        Input array.
+    out : ndarray, optional
+        Array into which the output is placed. Its type is preserved and it
+        must be of the right shape to hold the output.
+
+    Returns
+    -------
+    out : (..., 3) ndarray
+        Angles as `(deg, min, sec)` as last dimension.
+
+    Examples
+    --------
+    >>> np.deg2dms(48.5)
+    array([ 48., 30., 0.])
+    >>> np.deg2dms(-48.5)
+    array([-48., -30., -0.])
+    >>> np.dms2deg(np.deg2dms(-48.5))
+    -48.5
+
+    """
+
+    x = np.asarray(x)
+
+    if out is None:
+        out = np.empty(x.shape + (3, ), dtype=np.double)
+
+    xabs = np.abs(x)
+    out[..., 0] = np.floor(xabs)
+    xabs = (xabs - out[..., 0]) * 60
+    out[..., 1] = np.floor(xabs)
+    out[..., 2] = (xabs - out[..., 1]) * 60
+
+    sign = np.sign(x)
+    for i in range(3):
+        out[..., i] *= sign
+
+    return out
+
+
+def dms2deg(x, out=None):
+    """Convert angles in degrees, minutes and seconds to degrees.
+
+    Parameters
+    ----------
+    x : (..., 3) array_like
+        Input array as `(deg, min, sec)` as last dimension.
+    out : ndarray, optional
+        Array into which the output is placed. Its type is preserved and it
+        must be of the right shape to hold the output.
+
+    Returns
+    -------
+    out : ndarray
+        Angles in degrees.
+
+    Examples
+    --------
+    >>> np.dms2deg([48, 30, 0])
+    48.5
+    >>> np.dms2deg([-48, -30, 0])
+    -48.5
+    >>> np.deg2dms(np.dms2deg([-48, -30, 0]))
+    array([-48., -30., -0.])
+
+    """
+
+    x = np.asarray(x)
+
+    if out is None:
+        out = np.atleast_1d(np.empty(x.shape[:-1], dtype=np.double))
+
+    out[:] = x[..., 0] + x[..., 1] / 60. + x[..., 2] / 3600.
+
+    if x.ndim == 1:
+        return out[0]
+
+    return out
